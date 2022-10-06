f55b9cae1ba13a71bb69efc9a4f8f85fd1626028
Ricardo Soto <ricardo.scanaveras@securitasdirect.es>
2022-10-06 16:43:01 +0200

    empty spring boot project

---
+++ src/main/java/com/example/HelloTDD/HelloTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloTDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HelloTDD/HelloTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloTDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

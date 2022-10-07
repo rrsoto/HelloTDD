9b2e5e929484f5dabc81c93a6e451eff8fa0799f
Ricardo Soto <ricardo.scanaveras@securitasdirect.es>
2022-10-07 17:35:31 +0200

    feat: iteration 11 green

--- src/main/java/com/example/hellotdd/MyStack.java
+++ src/main/java/com/example/hellotdd/MyStack.java
@@ -7,22 +7,20 @@ public class MyStack {
   }
 
   private int size = 0;
-  private int pushedElement;
+  private int[] elements = new int[2];
 
   public boolean isEmpty() {
     return 0 == size;
   }
 
   public void push(int element) {
-    size++;
-    pushedElement = element;
+    elements[size++] = element;
   }
 
   public int pop() {
     if (0 == size)
       throw new StackUnderflowException();
-    size--;
-    return pushedElement;
+    return elements[--size];
   }
 
   public int size() {

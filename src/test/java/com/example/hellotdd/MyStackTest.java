73084ce42e8b1d1aa53c23a86eb87e71455272a6
Ricardo Soto <ricardo.scanaveras@securitasdirect.es>
2022-10-07 17:06:11 +0200

    test: iteration 8 red

--- src/test/java/com/example/hellotdd/MyStackTest.java
+++ src/test/java/com/example/hellotdd/MyStackTest.java
@@ -1,6 +1,7 @@
 package com.example.hellotdd;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,12 +18,14 @@ class MyStackTest {
   @Test
   void after_create_stack_is_empty() {
     assertThat(myStack.isEmpty()).isTrue();
+    assertThat(myStack.size()).isZero();
   }
 
   @Test
   void after_push_stack_is_not_empty() {
     myStack.push(0);
     assertThat(myStack.isEmpty()).isFalse();
+    assertThat(myStack.size()).isEqualTo(1);
   }
 
   @Test
@@ -38,6 +41,11 @@ class MyStackTest {
     myStack.push(0);
     myStack.push(0);
     assertThat(myStack.size()).isEqualTo(2);
+  }
+
+  @Test
+  void pop_with_empty_stack_throws_underflow(){
+    assertThatExceptionOfType(MyStack.Underflow.class).isThrownBy(myStack.pop());
   }
 
 }

6c8cbac7278e01311983eebfa9364e742d16b781
Ricardo Soto <ricardo.scanaveras@securitasdirect.es>
2022-10-07 14:47:13 +0200

    test: iteration 2 red

--- src/test/java/com/example/hellotdd/MyStackTest.java
+++ src/test/java/com/example/hellotdd/MyStackTest.java
@@ -1,11 +1,13 @@
 package com.example.hellotdd;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MyStackTest {
 
   @Test
   void test_create_stack() {
     MyStack myStack = new MyStack();
+    assertTrue(myStack.isEmpty());
   }
 }

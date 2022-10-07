25ff4a65dd6d6c3aff345e91a623f8239b6f5774
Ricardo Soto <ricardo.scanaveras@securitasdirect.es>
2022-10-07 15:03:07 +0200

    feat: iteration 4 green adds empty field

--- src/main/java/com/example/hellotdd/MyStack.java
+++ src/main/java/com/example/hellotdd/MyStack.java
@@ -1,11 +1,13 @@
 package com.example.hellotdd;
 
 public class MyStack {
+  private boolean empty = true;
 
   public boolean isEmpty() {
-    return true;
+    return empty;
   }
 
   public void push(int element) {
+    empty = false;
   }
 }
